44b1b5444ef0a02e8ec127b68ea9e4984c69bcb0
Anant7393 <anantmishra7393@gmail.com>
2021-09-08 21:10:23 +0530

    updated

---
+++ index.js
@@ -0,0 +1,31 @@
+
+         const texts=document.querySelector(".animate-text").children,
+         textsLen=texts.length;
+     let index=0;
+    const textInTimer=3000,
+          textOutTimer=2800;
+
+   function animateText() {
+      for(let i=0; i<textsLen; i++){
+        texts[i].classList.remove("text-in","text-out");  
+      }
+      texts[index].classList.add("text-in");
+
+      setTimeout(function(){
+          texts[index].classList.add("text-out");              
+      },textOutTimer)
+
+      setTimeout(function(){
+
+        if(index == textsLen-1){
+            index=0;
+          }
+         else{
+             index++;
+           }
+          animateText();
+      },textInTimer); 
+   }
+   
+   window.onload=animateText;
+
